04d618caffc2507a14bb035512caa61d3f04b1bc
Josef Šupka <supkajos@fit.cvut.cz>
2019-05-27 04:01:45 +0200

    StatsUpdater completed

--- src/Service/StatsUpdater.php
+++ src/Service/StatsUpdater.php
@@ -39,7 +39,7 @@ class StatsUpdater
                                           FROM (SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id
                                                 FROM game 
                                                 UNION 
-                                                SELECT season, is_autumn, id, league_id, AR2_official_id  AS official_id
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id
                                                 FROM game) t 
                                           GROUP BY season, is_autumn, league_id, official_id');
 
@@ -52,7 +52,7 @@ class StatsUpdater
                                                 SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id
                                                 FROM game 
                                                 UNION 
-                                                SELECT season, is_autumn, id, league_id, AR2_official_id  AS official_id
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id
                                                 FROM game) t 
                                           GROUP BY season, is_autumn, league_id, official_id');
 
@@ -130,16 +130,107 @@ class StatsUpdater
                                           ON game.id = yellow_card.game_id
                                           GROUP BY game.season, game.is_autumn, game.league_id, game.assessor_id');
 
+        $this->connection->executeUpdate('DELETE FROM stat_official_team');
+        $this->connection->executeUpdate('INSERT INTO stat_official_team (season, is_autumn, number_of_matches, league_id, official_id, team_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, official_id, team_id 
+                                          FROM (SELECT season, is_autumn, id, league_id, referee_official_id AS official_id, home_team_id AS team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, referee_official_id AS official_id, away_team_id AS team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id, home_team_id AS team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id, away_team_id AS team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id, home_team_id AS team_id
+                                                FROM game
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id, away_team_id AS team_id
+                                                FROM game) t 
+                                          GROUP BY season, is_autumn, league_id, official_id, team_id');
 
+        $this->connection->executeUpdate('DELETE FROM stat_official_home_team');
+        $this->connection->executeUpdate('INSERT INTO stat_official_home_team (season, is_autumn, number_of_matches, league_id, official_id, team_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, official_id, home_team_id 
+                                          FROM (SELECT season, is_autumn, id, league_id, referee_official_id AS official_id, home_team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id, home_team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id, home_team_id
+                                                FROM game) t 
+                                          GROUP BY season, is_autumn, league_id, official_id, home_team_id');
 
+        $this->connection->executeUpdate('DELETE FROM stat_referee_ar');
+        $this->connection->executeUpdate('INSERT INTO stat_referee_ar (season, is_autumn, number_of_matches, league_id, referee_official_id, AR_official_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, referee_official_id, AR_official_id 
+                                          FROM (SELECT season, is_autumn, id, league_id, referee_official_id, AR1_official_id AS AR_official_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, referee_official_id, AR2_official_id AS AR_official_id
+                                                FROM game) t 
+                                          GROUP BY season, is_autumn, league_id, referee_official_id, AR_official_id');
 
+        $this->connection->executeUpdate('DELETE FROM stat_official_official');
+        $this->connection->executeUpdate('INSERT INTO stat_official_official (season, is_autumn, number_of_matches, league_id, official_id1, official_id2)
+                                          SELECT season, is_autumn, COUNT(id), league_id, official_id1, official_id2 
+                                          FROM (SELECT season, is_autumn, id, league_id, referee_official_id AS official_id1, AR1_official_id AS official_id2
+                                                FROM game
+                                                WHERE referee_official_id > AR1_official_id
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id1, referee_official_id AS official_id2
+                                                FROM game
+                                                WHERE AR1_official_id > referee_official_id
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, referee_official_id AS official_id1, AR2_official_id AS official_id2
+                                                FROM game
+                                                WHERE referee_official_id > AR2_official_id
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id1, referee_official_id AS official_id2
+                                                FROM game
+                                                WHERE AR2_official_id > referee_official_id
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id1, AR2_official_id AS official_id2
+                                                FROM game
+                                                WHERE AR1_official_id > AR2_official_id
+                                                UNION
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id1, AR1_official_id AS official_id2
+                                                FROM game
+                                                WHERE AR2_official_id > AR1_official_id) t
+                                          GROUP BY season, is_autumn, league_id, official_id1, official_id2');
 
+        $this->connection->executeUpdate('DELETE FROM stat_referee_assessor');
+        $this->connection->executeUpdate('INSERT INTO stat_referee_assessor (season, is_autumn, number_of_matches, league_id, official_id, assessor_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, referee_official_id, assessor_id 
+                                          FROM game 
+                                          GROUP BY season, is_autumn, league_id, referee_official_id, assessor_id');
 
+        $this->connection->executeUpdate('DELETE FROM stat_official_assessor');
+        $this->connection->executeUpdate('INSERT INTO stat_official_assessor (season, is_autumn, number_of_matches, league_id, official_id, assessor_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, official_id, assessor_id 
+                                          FROM (SELECT season, is_autumn, id, league_id, referee_official_id AS official_id, assessor_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR1_official_id AS official_id, assessor_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, AR2_official_id AS official_id, assessor_id
+                                                FROM game) t 
+                                          GROUP BY season, is_autumn, league_id, official_id, assessor_id');
 
-
-
-
-
+        $this->connection->executeUpdate('DELETE FROM stat_assessor_team');
+        $this->connection->executeUpdate('INSERT INTO stat_assessor_team (season, is_autumn, number_of_matches, league_id, assessor_id, team_id)
+                                          SELECT season, is_autumn, COUNT(id), league_id, assessor_id, team_id 
+                                          FROM (SELECT season, is_autumn, id, league_id, assessor_id, home_team_id AS team_id
+                                                FROM game 
+                                                UNION 
+                                                SELECT season, is_autumn, id, league_id, assessor_id, away_team_id AS team_id
+                                                FROM game) t 
+                                          GROUP BY season, is_autumn, league_id, assessor_id, team_id');
 
     }
 
