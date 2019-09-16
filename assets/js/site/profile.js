8de252b7033ecf3079137475d4d6ac375cb6b611
Josef Šupka <josef.supka@gmail.com>
2019-09-16 02:24:48 +0200

    Site: show/hide stats button

--- assets/js/site/profile.js
+++ assets/js/site/profile.js
@@ -65,3 +65,15 @@ $(document).ready(function() {
 } );
 
 // ------------------------ END OF DATATABLES ------------------------
+
+// show/hide stats button
+$('#show_hide').on('click', function () {
+    if( $(this).data('show') ) { // show on last click
+        $('.collapse:not(#menu)').collapse('hide');
+        $(this).data('show', false); // set the data
+    }
+    else { // hide on last click
+        $('.collapse:not(#menu)').collapse('show');
+        $(this).data('show', true); // set the data
+    }
+});
--- assets/js/site/season_stats.js
+++ assets/js/site/season_stats.js
@@ -50,3 +50,15 @@ $(document).ready(function() {
         $('.loading').hide();
     });
 } );
+
+// show/hide stats button
+$('#show_hide').on('click', function () {
+        if( $(this).data('show') ) { // show on last click
+            $('.collapse:not(#menu)').collapse('hide');
+            $(this).data('show', false); // set the data
+        }
+        else { // hide on last click
+            $('.collapse:not(#menu)').collapse('show');
+            $(this).data('show', true); // set the data
+        }
+});
